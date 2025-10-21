a3477f7e32f0c85e506096d10bf46db799018864
Nicolo <nicoloridulfo@gmail.com>
2025-10-21 14:08:35 +0200

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Odin development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          odin
+          ols
+        ];
+      };
+    };
+}
