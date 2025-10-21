5e848f2b27f30713e5e33f5921304dd5cac7e12c
Nicolo <nicoloridulfo@gmail.com>
2025-10-21 14:15:08 +0200

    Make package nixos installable

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Odin development environment";
+  description = "A terminal based spreadsheet editor and calculator";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -7,14 +7,55 @@
 
   outputs = { self, nixpkgs }:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgsFor = system: nixpkgs.legacyPackages.${system};
     in {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          odin
-          ols
-        ];
-      };
+      packages = forAllSystems (system:
+        let
+          pkgs = pkgsFor system;
+        in {
+          default = pkgs.stdenv.mkDerivation {
+            pname = "spreadsheet";
+            version = "0.1.0";
+
+            src = ./.;
+
+            nativeBuildInputs = [ pkgs.odin ];
+
+            buildPhase = ''
+              runHook preBuild
+              odin build . -out:spreadsheet
+              runHook postBuild
+            '';
+
+            installPhase = ''
+              runHook preInstall
+              mkdir -p $out/bin
+              cp spreadsheet $out/bin/
+              runHook postInstall
+            '';
+
+            meta = with pkgs.lib; {
+              description = "A terminal based spreadsheet editor and calculator";
+              homepage = "https://github.com/dagis/spreadsheet";
+              license = licenses.gpl3Plus;
+              platforms = platforms.unix;
+              mainProgram = "spreadsheet";
+            };
+          };
+        });
+
+      devShells = forAllSystems (system:
+        let
+          pkgs = pkgsFor system;
+        in {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              odin
+              ols
+            ];
+          };
+        });
     };
 }
